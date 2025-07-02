fc3a4490a44d0539380c33d13dfc51a0358a4778
khana-kaye <khanakaye4@gmail.com>
2025-07-02 11:55:33 +0300

    Add scaffold and AppBar for ProfilePage with placeholder for bottom navigation

--- lib/screens/vendor/profile.dart
+++ lib/screens/vendor/profile.dart
@@ -33,3 +33,26 @@ class _ProfilePageState extends State<ProfilePage> {
       if (newEmail != null) email = newEmail;
     });
   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        backgroundColor: Colors.red[400],
+        title: const Text('Mak Bites'),
+    // leading: IconButton(
+    //   icon: const Icon(Icons.arrow_back),
+    //   onPressed: () => Navigator.pop(context),
+    // ),
+    ),
+    // bottomNavigationBar: BottomNavigationBar(
+    //   currentIndex: 3,
+    //   onTap: (index) {
+    //     // Handle navigation
+    //   },
+    //   items: const [
+    //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+    //     BottomNavigationBarItem(icon: Icon(Icons.restaurant_menu), label: 'Menu'),
+    //     BottomNavigationBarItem(icon: Icon(Icons.shopping_bag), label: 'Orders'),
+    //     BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+    //   ],
+    // ),
