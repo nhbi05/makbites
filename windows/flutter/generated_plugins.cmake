bea6929ad74af11b6b7d7249548d82f6fcf906de
Your Name <nabukeerasumayah354@gmail.com>
2025-07-04 12:02:49 +0300

    resolving merge changes

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -7,11 +7,8 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   firebase_auth
   firebase_core
-<<<<<<< HEAD
   firebase_storage
-=======
   url_launcher_windows
->>>>>>> fe2755cf6d51af9627ff9d9191d03dcc6d6f98c1
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
